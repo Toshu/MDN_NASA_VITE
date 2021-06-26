a8ceee88f5750d0d6596cce5393a7556746011f9
Toshu Amashu <toshude@yahoo.de>
2021-06-26 20:03:14 +0200

    added required  packages

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,8 @@ import App from './components/App';
 
 render(<App />, document.getElementById('app'));
 
-if (module.hot) {
-  module.hot.accept(function() {
-    window.location.reload();
-  });
-}
+// if (module.hot) {
+//   module.hot.accept(function() {
+//     window.location.reload();
+//   });
+// }
